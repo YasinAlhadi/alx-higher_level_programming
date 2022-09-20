c6d87dba6945b4536e91be947084ba68f382dc16
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-09-20 19:26:15 +0000

    prints a string in uppercase followed by a new line

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/bim/python3
+def uppercase(str):
+    for c in str:
+        if (ord('a') <= ord(c) and ord(c) <= ord('z')):
+            c = chr(ord(c) - 32)
+        print('{}'.format(c), end='')
+    print('\n', end='')
