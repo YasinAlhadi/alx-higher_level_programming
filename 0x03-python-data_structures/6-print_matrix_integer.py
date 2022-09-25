daa93b75121a8164ca115f7c5aa6223258243764
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-09-25 11:41:57 +0000

    fix pycodestyle

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-     if len(matrix[0]) == 0:
-         print()
-     else:
+    if len(matrix[0]) == 0:
+        print()
+    else:
         for i in range(0, len(matrix)):
             for x in range(0, len(matrix[i])):
                 if x != len(matrix[i]) - 1:
