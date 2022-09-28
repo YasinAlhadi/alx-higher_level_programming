d8064cc17385469391e38ee862732830e40af6fd
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-09-28 18:04:41 +0000

    Update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,4 +3,4 @@ def update_dictionary(a_dictionary, key, value):
     if a_dictionary is not None:
         new_dict = a_dictionary
         new_dict[key] = value
-        return new_dict
+    return new_dict
