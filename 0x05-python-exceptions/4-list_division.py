1f9c399f628451b342f28fa0f11f2fa6401b98d2
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-10-11 01:20:16 +0000

    divides element by element 2 lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,6 +8,8 @@ def list_division(my_list_1, my_list_2, list_length):
             count += 1
         except (TypeError, ValueError):
             print("wrong type")
+            count =+ 1
+            result = 0
         except ZeroDivisionError:
             print("division by 0")
             count += 1
