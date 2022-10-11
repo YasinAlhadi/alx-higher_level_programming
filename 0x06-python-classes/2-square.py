1122d7c5e9ebb3e5a0841bd3b337017d33a8aa23
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-10-11 15:25:42 +0000

    Instantiation with optional

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Write a class Square"""
+
+
+class Square:
+    """A class Square that defines a square."""
+    def __init__(self, size=0):
+        """initailazing size"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
