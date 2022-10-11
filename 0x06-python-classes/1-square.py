35f2991e155a0cb352779253dfb5603930c2a8da
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-10-11 09:12:45 +0000

    define a square2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a class Square"""
+
+
+class Square:
+    """A class Square that defines a square."""
+    def __init__(self, size):
+        """initailazing size"""
+        self.__size = size
