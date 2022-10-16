867b4e64d0edac3bd4ccc4f022a0059bab98b0eb
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-10-16 22:00:36 +0000

    Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""``0-add_integer`` module:
+    contain add_ineger function 
+    return addition a and b 
+    a and b must be integers or floats
+    a and b must be first casted to integers if they are float"""
+
+
+def add_integer(a, b=98):
+    """``add_ineger()`` function
+    Returns an integer
+    the addition of a and b"""
+    if type(a) not in (int, float):
+        raise TypeError('a must be an integer')
+    if type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
+
+
+"""if __name__ == "__main__":
+    import doctest
+    doctest.tesetfile("tests/0-add_integer.txt")"""
