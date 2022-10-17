f90475c5483a0dc44abb2ef5d6fbe823173d269e
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-10-17 19:04:27 +0000

    How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,6 +10,7 @@ class Rectangle:
     Empty class Rectangle that defines a rectangle
     """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """ initailize width and hieght"""
         self.width = width
