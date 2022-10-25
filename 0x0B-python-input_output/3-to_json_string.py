87e6a6e5f310ab7eb5e8f4bdc4c7b74881ebff37
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-10-25 15:41:25 +0000

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    ``3-to_json_string`` module
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+        function ``to_json_string`` returns the
+        JSON representation of an object (string).
+    """
+    return json.dumps(my_obj)
