b17fa41b5a6620a38386c6c046925f5f3148660c
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-10-26 15:08:28 +0000

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    ``10-student`` module
+"""
+
+
+class Student:
+    """class  that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initailize instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+            that retrieves a dictionary representation of a Student instance.
+        """
+        if type(attrs) is list and all([type(x) == str for x in attrs]):
+            return {i: j for i, j in self.__dict__.items() if i in attrs}
+        else:
+            return self.__dict__.copy()
