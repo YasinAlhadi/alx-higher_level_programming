83ef437486e9ee25cb9ee4b52bad9127353085a1
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-10-30 15:39:03 +0000

    rectangle square

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -19,45 +19,48 @@ class Rectangle(Base):
         return self.__width
 
     @width.setter
-    def width(self, value):
-        self.validator("width", value)
-        self.__width = value
+    def width(self, width):
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = width
 
     @property
     def height(self):
         return self.__height
 
     @height.setter
-    def height(self, value):
-        self.validator("height", value)
-        self.__height = value
+    def height(self, height):
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0".format(name))
+        self.__height = height
 
     @property
     def x(self):
         return self.__x
 
-    @width.setter
-    def x(self, value):
-        self.validator("x", value)
-        self.__x = value
+    @x.setter
+    def x(self, x):
+        if type(x) != int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0".format(name))
+        self.__x = x
 
     @property
     def y(self):
         return self.__y
 
-    @width.setter
-    def y(self, value):
-        self.validator("y", value)
-        self.__y = value
-
-    def validator(sel, name, value, xy=True):
-        """method to validate value"""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if not xy and value <= 0:
-            raise ValueError("{} must be > 0".format(name))
-        if xy and value < 0:
-            raise ValueError("{} must be >= 0".format(name))
+    @y.setter
+    def y(self, y):
+        if type(y) != int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = y
 
     def area(self):
         """method area that returns the area value of the Rectangle instance"""
@@ -66,5 +69,45 @@ class Rectangle(Base):
     def display(self):
         """that prints in stdout the Rectangle
         instance with the character '#'."""
-        rect = (("#" * self.width + '\n') * self.__height)[:-1]
-        print(rect)
+        rect = "\n" * self.__y + (" " * self.x + "#" * self.width + '\n') *\
+        self.__height
+        print(rect, end="")
+
+    def __str__(self):
+        """method that returns [Rectangle]
+        (<id>) <x>/<y> - <width>/<height>"""
+        return "[{}] ({}) {}/{} - {}/{}".format(type(self).__name__, self.id,
+                self.x, self.y, self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        """method update that assigns an argument to each attribute"""
+        if len(args) > 0:
+            for i, arg in enumerate(args):
+                if i == 0:
+                   self.id = arg
+                if i == 1:
+                   self.width = arg
+                if i == 2:
+                    self.height = arg
+                if i == 3:
+                    self.x = arg
+                if i == 4:
+                    self.y = arg
+        elif len(kwargs) > 0:
+            for i, j in kwargs.items():
+                if i == 'id':
+                    self.id = j
+                if i == 'width':
+                    self.width = j
+                if i == 'height':
+                    self.height = j
+                if i == 'x':
+                    self.x = j
+                if i == 'y':
+                    self.y = j
+
+    def to_dictionary(self):
+        """ ``to_dictionary`` that returns the dictionary
+        representation of a Rectangle"""
+        return {'id': self.id, 'width': self.__width,
+                 'height': self.__height, 'x': self.__x, 'y': self.__y}
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""square mudole"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """define Square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initailize instances"""
+        super().__init__(size, size, x, y, id=None)
+ 
+    def __str__(self):
+        """method that returns [Square]
+        (<id>) <x>/<y> - <width>/<height>"""
+        return "[{}] ({}) {}/{} - {}".format(type(self).__name__, self.id,
+                self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        self.width =  size
+        self.height = size
+
+    def update(self, *args, **kwargs):
+        """method ``update`` that assigns an argument to each attribute"""
+        if len(args) > 0:
+            for i, arg in enumerate(args):
+                if i == 0:
+                   self.id = arg
+                if i == 1:
+                   self.size = arg
+                if i == 2:
+                    self.x = arg
+                if i == 3:
+                    self.y = arg
+        elif len(kwargs) > 0:
+            for i, j in kwargs.items():
+                if i == 'id':
+                    self.id = j
+                if i == 'size':
+                    self.size = j
+                if i == 'x':
+                    self.x = j
+                if i == 'y':
+                    self.y = j
+
+    def to_dictionary(self):
+        """metho ``to_dictionary`` that returns the dictionary
+        representation of a Square"""
+        return {'id': self.id, 'size': self.width, 'x': self.x, 'y': self.y}
