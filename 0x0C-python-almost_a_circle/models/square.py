1f223461a1354ccede1ae6e6b1abe90e4bd50c98
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-10-31 21:33:39 +0000

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,7 +14,7 @@ class Square(Rectangle):
         (<id>) <x>/<y> - <width>/<height>"""
         return "[{}] ({}) {}/{} - {}"\
             .format(type(self).__name__, self.id, self.x, self.y,
-                    self.width)
+                    self.size)
 
     @property
     def size(self):
