01a994b8a0820296b2421fdca00c4b2d05db592b
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-10-31 22:06:57 +0000

    Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """base mudole"""
+import json
+import os
 
 
 class Base:
@@ -13,3 +15,49 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ method ``to_dicctionart``that returns the JSON string
+        representation of list_dictionaries """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ ``class method save to file`` that writes the JSON string
+        representation of list_objs to a file"""
+        if list_objs is not None:
+            list_objs = [obj.to_dictionary() for obj in list_objs]
+        with open(cls.__name__ + '.json', 'w', encoding='utf-8') as f:
+            f.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ static metho From_json_string`` that returns the list
+        of the JSON string representation json_string """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ class method ``create`` that returns an instance
+        with all attributes already set """
+        if cls.__name__ == 'Rectangle':
+            new = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            new = cls(1)
+        cls.update(new, **dictionary)
+        return new
+
+    @classmethod
+    def load_from_file(cls):
+        """ class mehotd `` load_from_file`` that returns a list
+        of instances """
+        if not os.path.isfile(cls.__name__ + '.json'):
+            return []
+        with open(cls.__name__ + '.json', 'r', encoding='utf-8') as f:
+            return [cls.create(**d) for d in cls.from_json_string(f.read())]
