375f3328771de9c3be8b197e303a8fbde6c7c0d4
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-12-01 00:57:54 +0000

    My genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,5 @@
+--  list all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT tv_genres.name
+FROM tv_genres, tv_show_genres, tv_shows
+WHERE tv_genres.id = tv_show_genres.genre_id AND tv_show_genres.genre_id = tv_shows.id AND tv_shows.title = "Dexter"
+ORDER BY tv_genres.name ASC;
