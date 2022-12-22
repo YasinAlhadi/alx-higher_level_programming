c631e4aead897eb6853eb681cdc3bb910e287a3c
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-12-22 09:38:53 +0000

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
                 BY states.id ASC""".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1] == sys.argv[4]:
+            print(row)
     cur.close()
     db_con.close()
