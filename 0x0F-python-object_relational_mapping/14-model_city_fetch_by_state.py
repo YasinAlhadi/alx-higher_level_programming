f4e87c946f5b6be5cd0ce64ea75063487f936c66
YasinAlhadi <yasin.a.m.elhadi@gamil.com>
2022-12-27 05:28:21 +0200

    Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
     session = session()
     for state, city in session.query(State, City).\
             filter(State.id == City.state_id).order_by(City.id).all():
-        print("{} : ({}) {}".format(state.name, city.id, city.name))
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
