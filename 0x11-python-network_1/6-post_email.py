e6c23973d18d33acdb9f12642736ab2047ccf318
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-01-17 21:10:56 +0200

    POST an email 1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,5 +6,5 @@ import sys
 
 if __name__ == "__main__":
     payload = {'email': sys.argv[2]}
-    r = requests.post(sys.argv[1], params=payload)
+    r = requests.post(sys.argv[1], data=payload)
     print(r.text)
