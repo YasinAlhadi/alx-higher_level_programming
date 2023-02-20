1801502b523689d1e0c731551f57979f7a3f9d32
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-02-20 21:56:46 +0200

    Mon 20 Feb 2023 09:56:46 PM EET

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,9 @@
+$.ajax({
+    type: 'GET',
+    url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+    success: function (data) {
+        for (let i = 0; i < data['results'].length; i++) {
+            $('#list_movies').append('<li>' + data['results'][i]['title'] + '</li>');
+        }
+    }
+});
